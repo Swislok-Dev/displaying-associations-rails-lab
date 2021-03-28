43e5889f5bc7901ae10a3f5e7dfdd2f4e0d91e5e
Swislok-Dev <cmarker4@gmail.com>
2021-03-27 23:18:42 -0500

    Done.

--- db/migrate/20210328041653_add_foreign_key_to_song.rb
+++ db/migrate/20210328041653_add_foreign_key_to_song.rb
@@ -2,4 +2,6 @@ class AddForeignKeyToSong < ActiveRecord::Migration[5.0]
   def change
     add_column :songs, :artist_id, :integer
   end
+
+  # rails g migration AddForeignKeyToSong artist:integer
 end
